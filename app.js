686e2adc90c2486fe9c0a823023bf80da34acbb8
Mehmet-EDE <mehmetedeiletisimm@gmail.com>
2022-07-14 15:48:06 +0300

    ejs

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs');
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
 
 
-app.get('/', (eq, res) => {
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-    res.send(blog)
-})
+app.get('/', (req, res) => {
+    res.render("index");
+  });
+  app.get('/about', (req, res) => {
+    res.render("about");
+  });
+  app.get('/addPost', (req, res) => {
+    res.render("add_post");
+  })
 app.listen(5858, () => {
     console.log('çalıştı')
 })
